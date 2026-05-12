31a95f984f8f9444955c8e477e9c62564c7d1422
NicholasNPham <npham@sao10.com>
2026-05-12 12:51:25 -0400

    first lecture notes

---
+++ COP3503C/ConsoleApplication1/LECTURE1.1.cpp
@@ -0,0 +1,11 @@
+/**
+* 
+From code to Program
+- Preprocessing: Creates "units" of code for compiling.
+-Compiling: Ensures the units are valid C++ code
+- Linking: Combine valid C++ code into an executable
+
+
+
+
+ */
