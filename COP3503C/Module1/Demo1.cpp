a2d4c7b7b1ed94532861149d0572cc4d3fb8e821
NicholasNPham <phamn1000@gmail.com>
2026-05-12 20:06:51 -0400

    new module

---
+++ COP3503C/Module1/Demo1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int demo1()
+{ 
+	std::cout << "Hello World!\n";
+	return 0;
+
+}
---
+++ COP3503C/Module1/main.cpp
@@ -0,0 +1,13 @@
+// Module1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int demo1();
+
+int main()
+{
+    demo1();
+    return 0;
+}
+
