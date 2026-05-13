5eb41ec8a3c2675109139ce4d6e933d55277f8ed
NicholasNPham <phamn1000@gmail.com>
2026-05-12 20:12:42 -0400

    first coding demo done

--- COP3503C/Module1/Demo1.cpp
+++ COP3503C/Module1/Demo1.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+ using namespace std;
 
 int demo1()
 { 
-	std::cout << "Hello World!\n";
+	// << insertion operator
+	// std Standard Namespace
+	// :: Scope-Resolution Operator
+	std::cout << "Hello World!" << std::endl;
+	cout << "Welcome to C++ Programming!" << endl;
 	return 0;
 
 }
