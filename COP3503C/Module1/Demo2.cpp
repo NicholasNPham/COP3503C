ff905b099165f6ca22d4f7b0ccf5c8b9310e353b
NicholasNPham <phamn1000@gmail.com>
2026-05-12 20:27:53 -0400

    slimmed functions

--- COP3503C/Module1/Demo2.cpp
+++ COP3503C/Module1/Demo2.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int demo2()
-{
-
-	// Task: Get user input for array of values
-	// Check to see if the sum is positive or negative
+int get_numbers()
+{ 
 
 	int numbers[3];
 
-	// >> Extraction operator
-	// << Insertion operator
-	
-
 	// Get input for all of the numbers in our array
-	for (int i = 0; i < 3; i ++)
-	{ 
+	for (int i = 0; i < 3; i++)
+	{
 		cin >> numbers[i];
 	}
 
@@ -28,12 +21,32 @@ int demo2()
 	for (int i = 0; i < 3; i++)
 		sum += numbers[i];
 
+	return sum;
+
+}
+
+void positive_or_negative(int sum)
+{ 
 	if (sum > 0)
 		cout << "Sum is positive!" << endl;
 	else if (sum < 0)
 		cout << "Sum is negative!" << endl;
 	else
 		cout << "Sum is equal to zero!" << endl;
+
+}
+
+int demo2()
+{
+
+	// Task: Get user input for array of values
+	int sum = get_numbers();
+	// Check to see if the sum is positive or negative
+	positive_or_negative(sum);
+
+	// >> Extraction operator
+	// << Insertion operator
+	
 
 
 	return 0;
