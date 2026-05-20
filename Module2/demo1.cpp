d51a7e4cc71cb82f82e07d1f6acbc2c12a597261
NicholasNPham <phamn1000@gmail.com>
2026-05-19 20:54:38 -0400

    declared hero created two function def hero is now defaulted and print info created to print. and called in hero

--- Module2/demo1.cpp
+++ Module2/demo1.cpp
@@ -12,12 +12,38 @@ class Hero
 	int level;      // What level are they?
 	int experience; // Get experience points to level up!
 
+public:
+	// Prototype == Delcaration
+	Hero(); // Default Constructor
+
+	void PrintInfo();
+
 };
 
+// Function Definitions
+Hero::Hero()
+{
+	name = "nick";
+	strength = 1;
+	hitpoints = 1;
+	level = 1;
+	experience = 0;
+}
+
+void Hero::PrintInfo()
+{
+	cout << "Name " << name << endl;
+	cout << "Strength " << strength << endl;
+	cout << "Hitpoints " << hitpoints << endl;
+	cout << "level " << level << endl;
+	cout << "experience " << experience << endl;
+}
 
 int hero()
 {
-	Hero my_hero;
+	Hero my_hero; // An Instance of the Hero Class
+	my_hero.PrintInfo();
+
 
 	return 0;
 
