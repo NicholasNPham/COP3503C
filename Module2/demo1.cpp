5580b4f479e0688841814d86ba0c52d59540a8ba
NicholasNPham <phamn1000@gmail.com>
2026-05-19 20:43:15 -0400

    started demo 1 with new class

---
+++ Module2/demo1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int hero()
+{
+
+	// Data to represent our hero
+	string name;    // Who is this?
+	int strength;   // How strong are they?
+	int hitpoints;  // How much damage can they take?
+	int level;      // What level are they?
+	int experience; // Get experience points to level up!
+
+	return 0;
+
+}
