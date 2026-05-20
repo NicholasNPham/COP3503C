e1355bb2bb29e886cf44196b182ff3e24a906dc3
NicholasNPham <phamn1000@gmail.com>
2026-05-19 21:10:33 -0400

    call new args in class call

--- Module2/demo1.cpp
+++ Module2/demo1.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int hero()
 {
-	Hero my_hero; // An Instance of the Hero Class
+	Hero my_hero("Nick", 10, 25); // An Instance of the Hero Class
 	my_hero.PrintInfo();
 
-	Hero sidekick;
+	Hero sidekick("Nick Jr.", 100, 300);
 	sidekick.PrintInfo();
 
 	return 0;
