642e55ef6c840ea08150cd48bb2adf42373a2942
NicholasNPham <phamn1000@gmail.com>
2026-05-20 22:33:20 -0400

    typo year from years

--- Module2/Vehicle.h
+++ Module2/Vehicle.h
@@ -1,3 +1,31 @@
 #pragma once
 #include <string>
+using std::string;
 
+class Vehicle
+{
+	// data to represent our hero explicit private
+private:
+	string _make;
+	string _model;
+	unsigned int _year;
+	float _price;
+	unsigned int _mileage;
+
+public:
+	// prototype
+	Vehicle();
+	Vehicle(
+		string make,
+		string model,
+		unsigned int year,
+		float price,
+		unsigned int mileage);
+
+	void Display();
+
+	string GetYearMakeModel();
+
+	float GetPrice();
+
+};
