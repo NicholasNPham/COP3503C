07e93132e005614a4ee4a684e02a71fa3e2dbc92
NicholasNPham <phamn1000@gmail.com>
2026-05-21 00:47:24 -0400

    added two functions for get average price and dealership, separated the for loops to not work with nested loops for clarity not optimization

---
+++ Module2/Dealership.cpp
@@ -0,0 +1,41 @@
+#include "Dealership.h"
+#include <iostream>
+using namespace std;
+
+Dealership::Dealership(string name, unsigned int capacity)
+{
+	_name = name;
+	_capacity = capacity;
+}
+
+void Dealership::AddShowroom(Showroom s)
+{
+	if (_showrooms.size() == _capacity)
+	{
+		cout << "Dealership is full, can't add another showroom!" << endl;
+	}
+	else
+		_showrooms.push_back(s);
+}
+
+float Dealership::GetAveragePrice()
+{
+	// initialize total and count for average
+	float total = 0;
+	int count = 0;
+
+	// total loop
+	for (int i = 0; i < _showrooms.size(); i++)
+		total += _showrooms[i].GetInventoryValue();
+
+	// count loop
+	for (int i = 0; i < _showrooms.size(); i++)
+		count += _showrooms[i].GetVehicleList().size();
+
+	return total / count;
+}
+
+void Dealership::ShowInventory()
+{
+
+}
