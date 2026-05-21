47681a2d22631756ecb92cf4f16c81d9226cc4b5
NicholasNPham <phamn1000@gmail.com>
2026-05-20 23:42:32 -0400

    created last 3 functions to get veh list, get inv value and show inv

--- Module2/Showroom.cpp
+++ Module2/Showroom.cpp
@@ -15,5 +15,26 @@ void Showroom::AddVehicle(Vehicle v)
 		cout << "Showroom is full! Cannot add " << v.GetYearMakeModel() << endl;
 	else
 		_vehicles.push_back(v);
+}
 
+vector <Vehicle> Showroom::GetVehicleList()
+{
+	return _vehicles;
+}
+
+void Showroom::ShowInventory()
+{
+
+	for (int i = 0; i < _vehicles.size(); i++)
+		_vehicles[i].Display();
+}
+
+float Showroom::GetInventoryValue()
+{
+	float total_price_of_showroom = 0;
+
+	for (int i = 0; i < _vehicles.size(); i++)
+		total_price_of_showroom += _vehicles[i].GetPrice();
+
+	return total_price_of_showroom;
 }
