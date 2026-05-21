1ad4a973d3883c20e3b102a2747869cf0f594be7
NicholasNPham <phamn1000@gmail.com>
2026-05-20 22:36:20 -0400

    constructor 1 with defaults and con2 with parameters

---
+++ Module2/Vehicle.cpp
@@ -0,0 +1,22 @@
+#include "Vehicle.h"
+#include <string>
+using namespace std;
+
+Vehicle::Vehicle()
+{
+	_make = "COP3503";
+	_model = "Rust Bucket";
+	_year = 1900;
+	_price = 0;
+	_mileage = 0;
+}
+
+Vehicle::Vehicle(string make, string model, unsigned int year, float price, unsigned int mileage)
+{
+	_make = make;
+	_model = model;
+	_year = year;
+	_price = price;
+	_mileage = mileage;
+
+}
