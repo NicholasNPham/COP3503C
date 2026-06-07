93b241b27fdc2ac1f2d319b6e1c23b6fbe631517
NicholasNPham <phamn1000@gmail.com>
2026-06-06 23:53:43 -0400

    finished demo4

--- Module5/demo4.cpp
+++ Module5/demo4.cpp
@@ -15,15 +15,32 @@ void ExampleOne()
 		cout << numbers[i] << endl;
 }
 
-//void ExampleTwo()
-//{
-//
-//}
+void ExampleTwo()
+{
+	DynamicArray<int> dynArray;
+
+	for (int i = 0; i < 10; i++)
+		dynArray.Add(i);
+
+	// Remove all elements from the array
+	while (dynArray.getSize() > 0)
+	{
+		dynArray.Remove(); // Remove the last item from the list.
+	}
+
+	if (dynArray.getSize() == 0)
+		cout << "Cleared succesfully!" << endl;
+	else
+		cout << "Still have itmes in the list!" << endl;
+}
 
 int demo4() {
 
-	ExampleOne();
-	//ExampleTwo();
+	//ExampleOne();
+	cout << "here" << endl;
+	ExampleTwo();
+	cout << "here" << endl;
+
 
 	return 0;
 }
