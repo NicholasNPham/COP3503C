ddaf33c5efb0df011b416d18cf488cd207e5b69d
NicholasNPham <phamn1000@gmail.com>
2026-06-13 08:41:01 -0400

    starting with guards and class def

---
+++ Project1/LinkedList.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::endl;
+
+template <typename T>
+class LinkedList
+{
+Private:
+	// HEAD
+	// TAIL
+	// COUNT
+
+Public:
+	// NODE STRUCT
+
+};
