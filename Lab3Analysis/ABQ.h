67ec615c785478be774db1f872c4ba4b7b1161e4
NicholasNPham <npham@sao10.com>
2026-06-15 13:35:44 -0400

    making sure commits are correct

---
+++ Lab3Analysis/ABQ.h
@@ -0,0 +1,217 @@
+#pragma once
+#include <iostream>
+#include <stdexcept>
+using std::cout;
+using std::endl;
+
+// ============== CLASS DEFINITION ============== 
+template <typename T>
+class ABQ
+{
+
+	unsigned int _front;
+	unsigned int _size;
+	unsigned int _capacity;
+	unsigned int _total_resizes;
+	T* _data;
+	float _scaleFactor;
+
+public:
+
+	// ============== CONSTRUCTORS ============== 
+	ABQ();
+	ABQ(int capacity);
+	ABQ(int capacity, float scale_factor);
+	ABQ(const ABQ& d); // BIG THREE
+
+
+	// ============== COPY ASSIGNMENT OPERATOR ============== 
+	ABQ& operator=(const ABQ& d); // BIG THREE
+
+	// ============== DESTRUCTOR ============== 
+	~ABQ(); //BIG THREE
+
+	// ============== MUTATORS ==============
+	void enqueue(T data);
+	T dequeue();
+
+	// ============== ACCESSORS ============== 
+	T peek() const;
+	unsigned int getSize() const;
+	unsigned int getMaxCapacity() const;
+	unsigned int getTotalResizes() const;
+	T* getData() const;
+};
+
+// ============== CLASS MEMBER DEFINITIONS ==============
+
+template <typename T> // DEFAULT CONSTRUCTOR
+ABQ<T>::ABQ()
+{
+	_front = 0;
+	_size = 0;
+	_capacity = 1;
+	_total_resizes = 0;
+	_scaleFactor = 2.0f;
+	_data = new T[_capacity];
+}
+
+template <typename T> // PARAMETERIZED CONSTRUCTOR
+ABQ<T>::ABQ(int capacity)
+{
+	_front = 0;
+	_size = 0;
+	_capacity = capacity;
+	_total_resizes = 0;
+	_scaleFactor = 2.0f;
+	_data = new T[_capacity];
+}
+
+template <typename T>
+ABQ<T>::ABQ(int capacity, float scale_factor)
+{
+	_front = 0;
+	_size = 0;
+	_capacity = capacity;
+	_total_resizes = 0;
+	_scaleFactor = scale_factor;
+	_data = new T[_capacity];
+}
+
+template <typename T> // COPY CONSTRUCTOR
+ABQ<T>::ABQ(const ABQ& d)
+{
+	// MEMBER VARIABLES
+	_front = d._front;
+	_size = d._size;
+	_capacity = d._capacity;
+	_total_resizes = d._total_resizes;
+	_scaleFactor = d._scaleFactor;
+
+	_data = new T[_capacity]; // DEEP COPY
+
+	// COPY THE DATA OVER
+	for (unsigned int i = 0; i < d._size; i++)
+		_data[i] = d._data[i];
+}
+
+template <typename T> // COPY ASSIGNMENT OPERATOR
+ABQ<T>& ABQ<T>::operator=(const ABQ& d)
+{
+	if (this != &d) //DOES THE CURRENT OBJECT LIVE AT THE SAME ADDRESS AS D
+	{
+		delete[] _data;
+
+		// MEMBER VARIABLES
+		_front = d._front;
+		_size = d._size;
+		_capacity = d._capacity;
+		_total_resizes = d._total_resizes;
+		_scaleFactor = d._scaleFactor;
+
+		_data = new T[_capacity];
+
+		// COPY THE DATA OVER
+		for (unsigned int i = 0; i < d._size; i++)
+			_data[i] = d._data[i];
+	}
+
+	return *this;
+
+}
+
+template <typename T> // DESTRUCTOR
+ABQ<T>::~ABQ()
+{
+	delete[] _data;
+}
+
+template <typename T>
+void ABQ<T>::enqueue(T data)
+{
+	if (_capacity == _size) // CHECK IF FULL
+	{
+
+		unsigned int oldCapacity = _capacity; // SAVE THE OLD CAPACITY BEFORE CHANGE
+
+		_capacity *= _scaleFactor; // INCREASE THE CAPACITY
+		T* newArray = new T[_capacity]; // CREATE A NEW ARRAY
+
+		// COPY DATA OVER
+		for (unsigned int i = 0; i < _size; i++)
+			newArray[i] = _data[(_front + i) % oldCapacity];
+
+		_front = 0; // SET FRONT TO INDEX 0
+		delete[] _data; // DELETE OLD DATA
+		_data = newArray; // POINT TO NEW ARRAY
+
+		_total_resizes++;
+	}
+
+	_data[(_front + _size) % _capacity] = data; // ADD TO THE STACK IN THE BACK
+	_size++; // UPDATE SIZE
+
+}
+
+template <typename T>
+T ABQ<T>::dequeue()
+{
+	if (_size == 0)
+	{
+		throw std::runtime_error("ARRAY IS EMPTY NOTHING TO REMOVED");
+	}
+	T tempVar = _data[_front];
+	_front = (_front + 1) % _capacity; // FRONT IS WHATEVER THE NEXT INDEX IS
+	_size--; // SHRINK THE SIZE
+
+	if (_size < (_capacity / _scaleFactor))
+	{
+		unsigned int oldCapacity = _capacity; // SAVE THE OLD CAPACITY BEFORE CHANGE
+		_capacity /= _scaleFactor;
+
+		T* newArray = new T[_capacity]; // CREATE A NEW ARRAY
+
+		// COPY DATA OVER
+		for (unsigned int i = 0; i < _size; i++)
+			newArray[i] = _data[(_front + i) % oldCapacity];
+
+		_front = 0; // FRONT IS NOW INDEX 0
+		delete[] _data; // DELETE OLD DATA
+		_data = newArray; // POINT TO NEW ARRAY
+		
+		_total_resizes++;
+	}
+	return tempVar;
+}
+
+template <typename T>
+T ABQ<T>::peek() const {
+
+	if (_size == 0)
+	{
+		throw std::runtime_error("ARRAY IS EMPTY NOTHING TO PEEK");
+	}
+
+	return _data[_front]; // THIS RETURNS THE VALUE ON THE TOP OF THE LIST	
+
+}
+
+template <typename T>
+unsigned int ABQ<T>::getSize() const {
+	return _size;
+}
+
+template <typename T>
+unsigned int ABQ<T>::getMaxCapacity() const {
+	return _capacity;
+}
+
+template <typename T>
+unsigned int ABQ<T>::getTotalResizes() const {
+	return _total_resizes;
+}
+
+template <typename T>
+T* ABQ<T>::getData() const {
+	return _data;
+}
---
+++ Lab3Analysis/ABS.h
@@ -0,0 +1,202 @@
+#pragma once
+#include <iostream>
+#include <stdexcept>
+using std::cout;
+using std::endl;
+
+// ============== CLASS DEFINITION ============== 
+template <typename T>
+class ABS
+{
+
+	unsigned int _size;
+	unsigned int _capacity;
+	unsigned int _total_resizes;
+	T* _data;
+	float _scaleFactor;
+
+public:
+
+// ============== CONSTRUCTORS ============== 
+	ABS();
+	ABS(int capacity);
+	ABS(int capacity, float scale_factor);
+	ABS(const ABS& d); // BIG THREE
+
+
+// ============== COPY ASSIGNMENT OPERATOR ============== 
+	ABS& operator=(const ABS& d); // BIG THREE
+
+// ============== DESTRUCTOR ============== 
+	~ABS(); //BIG THREE
+
+// ============== MUTATORS ==============
+	void push(T data);
+	T pop();
+
+// ============== ACCESSORS ============== 
+	T peek() const;
+	unsigned int getSize() const;
+	unsigned int getMaxCapacity() const;
+	unsigned int getTotalResizes() const;
+	T* getData() const;
+};
+
+// ============== CLASS MEMBER DEFINITIONS ==============
+
+template <typename T> // DEFAULT CONSTRUCTOR
+ABS<T>::ABS()
+{
+	_size = 0;
+	_capacity = 1;
+	_total_resizes = 0;
+	_scaleFactor = 2.0f;
+	_data = new T[_capacity];
+}  
+
+template <typename T> // PARAMETERIZED CONSTRUCTOR
+ABS<T>::ABS(int capacity)
+{
+		_size = 0;
+		_capacity = capacity;
+		_total_resizes = 0;
+		_scaleFactor = 2.0f;
+		_data = new T[_capacity];
+}
+
+template <typename T>
+ABS<T>::ABS(int capacity, float scale_factor) 
+{
+	_size = 0;
+	_capacity = capacity;
+	_total_resizes = 0;
+	_scaleFactor = scale_factor;
+	_data = new T[_capacity];
+}
+
+template <typename T> // COPY CONSTRUCTOR
+ABS<T>::ABS(const ABS& d)
+{
+	// MEMBER VARIABLES
+	_size = d._size;
+	_capacity = d._capacity;
+	_total_resizes = d._total_resizes;
+	_scaleFactor = d._scaleFactor;
+
+	_data = new T[_capacity]; // DEEP COPY
+
+	// COPY THE DATA OVER
+	for (unsigned int i = 0; i < d._size; i++)
+		_data[i] = d._data[i];
+}
+
+template <typename T> // COPY ASSIGNMENT OPERATOR
+ABS<T>& ABS<T>::operator=(const ABS& d)
+{
+	if (this != &d) //DOES THE CURRENT OBJECT LIVE AT THE SAME ADDRESS AS D
+	{
+		delete[] _data;
+		
+		// MEMBER VARIABLES
+		_size = d._size;
+		_capacity = d._capacity;
+		_scaleFactor = d._scaleFactor;
+		_total_resizes = d._total_resizes;
+
+		_data = new T[_capacity];
+
+		// COPY THE DATA OVER
+		for (unsigned int i = 0; i < d._size; i++)
+			_data[i] = d._data[i];
+	}
+
+	return *this;
+
+}
+
+template <typename T> // DESTRUCTOR
+ABS<T>::~ABS()
+{
+	delete[] _data;
+}
+
+template <typename T>
+void ABS<T>::push(T data)
+{
+	if (_capacity == _size) // CHECK IF FULL
+	{
+		_capacity *= _scaleFactor; // INCREASE THE CAPACITY
+		T* newArray = new T[_capacity]; // CREATE A NEW ARRAY
+		_total_resizes++;
+	
+		// COPY DATA OVER
+		for (unsigned int i = 0; i < _size; i++)
+			newArray[i] = _data[i];
+
+		delete[] _data; // DELETE OLD DATA
+		_data = newArray; // POINT TO NEW ARRAY
+	}
+
+	_data[_size] = data; // ADD TO THE STACK
+	_size++; // UPDATE SIZE
+
+}
+
+template <typename T>
+T ABS<T>::pop()
+{
+	if (_size == 0)
+	{
+		throw std::runtime_error("ARRAY IS EMPTY NOTHING TO REMOVED");
+	}
+	T tempVar = _data[_size - 1];
+	_size--;
+
+	if (_size < (_capacity / _scaleFactor))
+	{
+		 _capacity /= _scaleFactor;
+
+		T* newArray = new T[_capacity]; // CREATE A NEW ARRAY
+
+		// COPY DATA OVER
+		for (unsigned int i = 0; i < _size; i++)
+			newArray[i] = _data[i];
+
+		delete[] _data; // DELETE OLD DATA
+		_data = newArray; // POINT TO NEW ARRAY
+		_total_resizes++;
+	}
+	return tempVar;
+}
+
+template <typename T>
+T ABS<T>::peek() const {
+
+	if (_size == 0)
+	{
+		throw std::runtime_error("ARRAY IS EMPTY NOTHING TO PEEK");
+	}
+
+	return _data[_size - 1]; // THIS RETURNS THE VALUE ON THE TOP OF THE LIST	
+
+}
+
+template <typename T>
+unsigned int ABS<T>::getSize() const {
+	return _size;
+}
+
+template <typename T>
+unsigned int ABS<T>::getMaxCapacity() const {
+	return _capacity;
+}
+
+template <typename T>
+unsigned int ABS<T>::getTotalResizes() const {
+	return _total_resizes;
+}
+
+template <typename T>
+T* ABS<T>::getData() const {
+	return _data;
+}
