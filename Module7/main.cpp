31fc9a0c4077eb1d70f4dc5fde2913e6dade4a75
NicholasNPham <npham@sao10.com>
2026-06-19 10:17:46 -0400

    finished demo 4 part 1

--- Module7/main.cpp
+++ Module7/main.cpp
@@ -2,27 +2,65 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include "Hero.h"
 using namespace std;
+
+void ReadFromHeroesFile(string filePath, vector<Hero>& output)
+{
+
+	// READ A DATA FILE WITH A COUNT
+	ifstream inFile(filePath);
+	if (inFile.is_open())
+	{
+		// 1. READ THE COUNT
+		int count;
+
+		string dataFromFile;
+		getline(inFile, dataFromFile); // READ 3
+
+		// CONVERT TO AN INTEGER
+		count = stoi(dataFromFile);
+		for (int i = 0; i < count; i++)
+		{
+			string name;
+			getline(inFile, name); // READ 3
+
+
+			// GET THE STRENGTH VALUE
+			getline(inFile, dataFromFile);
+			int strength = stoi(dataFromFile);
+
+			// GET THE HITPOINTS VALUE
+			getline(inFile, dataFromFile);
+			int hitpoints = stoi(dataFromFile);
+
+			Hero heroObjectFromFileData(name, strength, hitpoints);
+			output.push_back(heroObjectFromFileData);
+		}
+	}
+	else
+	{
+		cout << "Error! File " << filePath << " Could not be opened!" << endl;
+	}
+}
 
 int main()
 {
-	vector<int> numbers;
-	ifstream inFile("demo3/file2.txt");
+	vector<Hero> heroes;
+	ReadFromHeroesFile("data/info.heroes", heroes);
 
-	int someValue;
-	
-	while (!inFile.eof())
+	// 2. FOR (COUNT)
+	for (unsigned int i = 0; i < heroes.size(); i++)
 	{
-		inFile >> someValue;
-
-		numbers.push_back(someValue);
+		heroes[i].PrintInfo();
 	}
 	
+	// 3. READ INDIVIDUAL OBJECTS
 
-	cout << "numbers from file: " << numbers.size() << endl;
-	for (unsigned int i = 0; i < numbers.size(); i++)
-	{
-		cout << numbers[i] << ' ';
-	}
+	// READ A CSV FILE
+	ifstream csv("data/heroes.csv");
+
 	return 0;
 }
+
+
