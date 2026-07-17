db15fba34e345f61f62bc8d14538bd4ee6556e3e
NicholasNPham <npham@sao10.com>
2026-07-17 16:45:06 -0400

    defined prototypes

--- P2/src/image.h
+++ P2/src/image.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 using std::string;
+using std::vector;
 
 struct Header
 {
@@ -18,6 +19,30 @@ struct Header
 	char pixelDepth;
 	char imageDescriptor;
 
+	bool operator==(const Header& rhs) const {
+
+		if (idLength == rhs.idLength &&
+				colorMapType == rhs.colorMapType &&
+				imageType == rhs.imageType &&
+				colorMapOrigin == rhs.colorMapOrigin &&
+				colorMapLength == rhs.colorMapLength &&
+				colorMapDepth == rhs.colorMapDepth &&
+				xOrigin == rhs.xOrigin &&
+				yOrigin == rhs.yOrigin &&
+				imageWidth == rhs.imageWidth &&
+				imageHeight == rhs.imageHeight &&
+				pixelDepth == rhs.pixelDepth &&
+				imageDescriptor == rhs.imageDescriptor)
+		{ 
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+
 };
 
 class Image
@@ -32,8 +57,14 @@ public:
 	short getWidth();
 	short getHeight();
 	unsigned char getPixel(int pixelIndex, int channel);
+	vector<unsigned char> getPixelVector();
+	Header getHeader();
 
 	// mutator ------------------------
 	void setPixel(int pixelIndex, int channel, unsigned char newPixelValue);
 
 };
+
+bool compareImages(Image& img1, Image& img2);
+bool runSingleTest(Image& img1, Image& img2, string testNum);
+void runAllTests();
