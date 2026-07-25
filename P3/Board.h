d22d6567aaaa155ecda0cec30d4d72d89c48a1a5
NicholasNPham <phamn1000@gmail.com>
2026-07-25 18:34:12 -0400

    prepped board heaader file

--- P3/Board.h
+++ P3/Board.h
@@ -1,5 +1,38 @@
 #pragma once
+#include <vector>
+#include <string>
+#include "Tile.h"
+using std::vector;
+using std::string;
+
 class Board
 {
+	vector<vector<Tile>> _gridOfTiles;
+	int _numOfRows;
+	int _numOfCols;
+	int _numOfMines;
+	int _mineMinusFlagCount;
+	bool _gameOver;
+	bool _gameLost;
+	bool _debugMode;
+public:
+	Board();
+	void loadConfigFile(string filename);
+	void loadBoardFile(string filename);
+	void resetGame();
+	void setRandomMines();
+	void calculateAdjacentMines();
+
+	// Acessors ------------------------
+	int getRowCount() const;
+	int getColCount() const;
+	int getMineMinusFlagCount() const;
+	const Tile& getTileState(int row, int col) const;
+
+	// Mutators ------------------------
+	void revealTile(int row, int col);
+	void toggleFlagPosition(int row, int col);
+	bool checkWin();
+	void toggleDebug();
 };
 
