bfa686518c49ae348662d702f833d443eeac206d
NicholasNPham <phamn1000@gmail.com>
2026-07-24 23:05:43 -0400

    prepped tile.h file

--- P3/Tile.h
+++ P3/Tile.h
@@ -1,5 +1,23 @@
 #pragma once
 class Tile
 {
-};
+	bool mineFlag;
+	bool flaggedFlag;
+	bool revealFlag;
+	int adjacentMines;
+public:
+	Tile();
 
+	// Accessors ---------------------
+	int getAdjacentMines() const;
+	bool isMine() const;
+	bool isFlagged() const;
+	bool isRevealed() const;
+
+	// Mutators ----------------------
+	void setMine(bool mine);
+	void toggleFlag();
+	void setAdjacentMines(int count);
+	void reveal();
+
+};
