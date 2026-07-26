29425397b25f10ed48cca6964ee66b4e64836073
NicholasNPham <phamn1000@gmail.com>
2026-07-25 23:15:01 -0400

    added logic for default constructor and loadConfigFile function

--- P3/Board.cpp
+++ P3/Board.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <fstream>
 #include "Board.h"
+using namespace std;
+
+Board::Board()
+{
+	_numOfRows = 0;
+	_numOfCols = 0;
+	_numOfMines = 0;
+	_mineMinusFlagCount = 0;
+	_gameOver = false;
+	_gameLost = false;
+	_debugMode = false;
+}
+
+void Board::loadConfigFile(string filename)
+{
+	ifstream file(filename);
+
+	if (!file.is_open())
+	{
+		cout << "Could not open File: " << filename << endl;
+		return;
+	}
+
+	file >> _numOfCols;
+	file >> _numOfRows;
+	file >> _numOfMines;
+}
