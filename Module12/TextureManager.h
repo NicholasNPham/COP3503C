8d3e1b7b38a5e44af86cd6262798d7f18bc1a9bf
NicholasNPham <npham@sao10.com>
2026-07-27 08:24:18 -0400

    m12

---
+++ Module12/TextureManager.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <unordered_map>
+#include <string>
+
+using std::string;
+using std::unordered_map;
+
+class TextureManager
+{
+	// static == one and only one of this thing
+	static unordered_map<string, sf::Texture> _textures;
+	static void LoadTexture(string textureName);
+	
+public:
+	static sf::Texture& GetTexture(string textureName);
+	static void Clear(); // Call this at the end of main 
+};
